2190ab2ba049543ba73d20485ebd4f58b2652d8e
Alexander Shepetko <a@shepetko.com>
2018-04-08 06:07:17 +0300

    Bugfix 2.0.2

--- README.md
+++ README.md
@@ -4,6 +4,11 @@
 ## Changelog
 
 
+### 2.0.2 (2018-04-08)
+
+JS code fixed.
+
+
 ### 2.0.1 (2018-04-08)
 
 Widgets initialization fixed.
--- res/assets/js/form.js
+++ res/assets/js/form.js
@@ -1,3 +1,3 @@
 require(['jquery'], function($) {
-    require(['twitter-bootstrap-' + $('#auth-ui-form').data('bsVersion')])
+    require(['twitter-bootstrap-' + $('.auth-ui-form').data('bsVersion')])
 });
